9394c10dd409c86618a57b5af4cad68f90d06f63
Vishank Singh <155613318+vishank020@users.noreply.github.com>
2025-12-07 15:01:50 +0530

    Change base path in Vite config to '/gitprofile/'

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: CONFIG.base || '/',
+  base: CONFIG.base || '/gitprofile/',
   plugins: [
     react(),
     createHtmlPlugin({
